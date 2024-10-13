666ce2798d376eb4f7efd3909287164cbed920e5
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2024-10-13 15:39:39 -0500

    Add skeleton for lab

---
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -0,0 +1,5 @@
+package com.csc;
+
+public class TriangleNumberCalculator {
+  // Your code goes here!
+}
---
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -0,0 +1,15 @@
+package com.csc;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestTriangleNumberCalculator {
+
+  TriangleNumberCalculator calculator;
+
+  @BeforeEach
+  void setUp() {
+    calculator = new TriangleNumberCalculator();
+  }
+
+  // Your tests go here
+}
